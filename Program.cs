a01729f7176d10cc056de804c67175d171a4b4fb
munohhh <143058333+munohhh@users.noreply.github.com>
2024-09-07 18:52:55 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patientName;
+            Console.WriteLine("Who is being seen today?"); 
+            patientName = Console.ReadLine();
+            Console.WriteLine("Welcome:" +  patientName);
+
+            
+       
+        }
+    }
+}
